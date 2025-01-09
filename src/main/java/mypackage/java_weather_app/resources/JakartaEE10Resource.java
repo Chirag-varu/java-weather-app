cbab8f91b9dc674a2815448ebebf44fd45c2f277
skymonil <monilparikh11@gmail.com>
2025-01-09 23:55:17 +0530

    first commit

---
+++ src/main/java/mypackage/java_weather_app/resources/JakartaEE10Resource.java
@@ -0,0 +1,20 @@
+package mypackage.java_weather_app.resources;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.core.Response;
+
+/**
+ *
+ * @author 
+ */
+@Path("jakartaee10")
+public class JakartaEE10Resource {
+    
+    @GET
+    public Response ping(){
+        return Response
+                .ok("ping Jakarta EE")
+                .build();
+    }
+}
